adbaa81adb124a42bfee3f6172d22d6d2cd606b7
John Marcel Silveira <marcel-eu@hotmail.com>
2023-06-20 07:50:50 -0300

    aprendendo array e objetos

---
+++ Array/atividades_arr/scripts.js
@@ -0,0 +1,10 @@
+let meuPrimeiroObjt = {
+  patas: 4,
+  nome: "Thor",
+  latir: function(){
+    console.log("Au, au");
+  }
+};
+
+console.log(meuPrimeiroObjt.nome);
+meuPrimeiroObjt.latir();
--- Function/atividades/scripts.js
+++ Function/atividades/scripts.js
@@ -11,3 +11,29 @@ function validarIdade(idade) {
 }
 
 validarIdade(30);
+
+function positivarNumero(numero) {
+  return numero * -1;
+}
+
+console.log(positivarNumero(-200));
+
+function contarCaracteres(texto) {
+  if (texto.length > 10) {
+    console.log("Texto muito longo");
+  } else {
+    console.log("Texto dentro do limite");
+  }
+}
+
+contarCaracteres("123456789");
+
+function imprimePares(numero) {
+  for (let i = numero; i > 0; i--) {
+    if (i % 2 == 0) {
+      console.log(i);
+    }
+  }
+}
+
+imprimePares(10);
